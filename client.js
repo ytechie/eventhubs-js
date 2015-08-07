b32f65ac1e214b0f2872f7080b9e16d395e367f3
Jason Young <jason@ytechie.com>
2015-08-07 18:33:03 -0500

    Display the device id to avoid confusion

--- client.js
+++ client.js
@@ -72,6 +72,8 @@ function sendMessage(options) {
         responseData = '';
 
 	deferral = Q.defer();
+    
+    console.log('Generating URI for device ID: ' + deviceId);
 
     deviceUri = getDeviceUri(deviceId);
     token = sasToken || getSasToken(deviceUri);
